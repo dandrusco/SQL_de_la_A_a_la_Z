fd18d4ae664b727a1a0e5221d946db3dc7b70590
dAndrusco <dandrusco@gmail.com>
2025-02-19 10:07:54 -0300

    Crear esquemas

---
+++ 03-Setencias de definición en SQL/04-Crear esquemas.sql
@@ -0,0 +1,19 @@
+/* 
+Creación de Esquemas
+========================
+
+CREATE SCHEMA 
+{[nombre_del_esquema] | [AUTHORIZATION usuario]}
+[lista_de_elementos_del_esquema]; 
+*/ 
+
+/* 
+Nomenclatura utilizada
+========================
+
+PALABRAS EN MAYUSCULAS: Son palabras reservadas del lenguaje, tambien se pueden poner en minusculas pero se recomiendaescribirlo en 
+						mayuzculas como CREATE SCHEMA 
+[]: Quiere decir que lo que esta en [] se puede poner, como no poner, por ejemplo [AUTHORIZATION usuario]
+{}: Son obligatoria {[nombre_del_esquema] | [AUTHORIZATION usuario]}
+|: Se puede elejir uno de los dos [nombre_del_esquema] | [AUTHORIZATION usuario]
+*/ 
